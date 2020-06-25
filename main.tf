4b75154f2c1da8880bfefa71aad0178203e58fec
Will <22464726+willgleich@users.noreply.github.com>
2020-06-24 22:23:52 -0600

    Init Commit of "Composting" AWS Infra

---
+++ main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-west-2"
+}
+
+terraform {
+  backend "consul" {
+    address = "consul.gleich.tech"
+    scheme  = "https"
+    path    = "tf/aws/network"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "maina" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "mainb" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-06c119f12fa66b35b"
+  instance_type = "t3.small"
+  subnet_id     = aws_subnet.mainb.id
+  security_groups = [aws_security_group.allow_all.id]
+  key_name = "OnPrem"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+
+resource "aws_subnet" "mainc" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-west-2c"
+
+  tags = {
+    Name = "Main"
+  }
+
+}
+
+resource "aws_customer_gateway" "main" {
+  bgp_asn    = 65000
+  ip_address = "97.117.69.79"
+  type       = "ipsec.1"
+
+  tags = {
+    Name = "main-customer-gateway"
+  }
+}
+
+
+resource "aws_vpn_gateway" "vpn_gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "all from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+//    cidr_blocks = [aws_vpc.main.cidr_block]
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+resource "aws_vpn_connection" "main" {
+  vpn_gateway_id      = "${aws_vpn_gateway.vpn_gw.id}"
+  customer_gateway_id = "${aws_customer_gateway.main.id}"
+  type                = "ipsec.1"
+  static_routes_only  = true
+
+  tags  = {
+          "Name" = "OPNsense"
+        }
+
+}
